ffdde5cf8793beb67ea2e14a27c6df359e3ea855
Unknown <alan.vidaurre@gmail.com>
2019-07-01 10:02:46 -0600

    Nuevo proyecto de vistas

---
+++ UdeoPrograXFinalWeb/CasaDollarWeb/CasaDollarWeb/Controllers/ClienteController.cs
@@ -0,0 +1,46 @@
+﻿using CasaDollarWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasaDollarWeb.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Clientes()
+        {
+            IEnumerable<Cliente> members = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:56746/api/cliente/");
+                
+                var responseTask = client.GetAsync("findall");
+                responseTask.Wait();
+
+                //To store result of web api response.   
+                var result = responseTask.Result;
+
+                //If success received   
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Cliente>>();
+                    readTask.Wait();
+
+                    members = readTask.Result;
+                }
+                else
+                {
+                    //Error response received   
+                    members = Enumerable.Empty<Cliente>();
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+            return View(members);
+        }
+    }
+}
---
+++ UdeoPrograXFinalWeb/CasaDollarWeb/CasaDollarWeb/Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CasaDollarWeb.Models
+{
+    public class Cliente
+    {
+
+        private int id;
+        private string primerNombre;
+        private string segundoNombre;
+        private string primerApellido;
+        private string segundoApellido;
+        private DateTime nacimiento;
+        private decimal ingresoMensual;
+
+        public int Id { get => id; set => id = value; }
+        public string PrimerNombre { get => primerNombre; set => primerNombre = value; }
+        public string SegundoNombre { get => segundoNombre; set => segundoNombre = value; }
+        public string PrimerApellido { get => primerApellido; set => primerApellido = value; }
+        public string SegundoApellido { get => segundoApellido; set => segundoApellido = value; }
+        public DateTime Nacimiento { get => nacimiento; set => nacimiento = value; }
+        public decimal IngresoMensual { get => ingresoMensual; set => ingresoMensual = value; }
+
+    }
+}
