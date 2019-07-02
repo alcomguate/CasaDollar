02f58314c97ba5c0ec9e589a892b6ede45eab71b
Unknown <alan.vidaurre@gmail.com>
2019-07-01 19:10:47 -0600

    Pantalla de gastos

---
+++ UdeoPrograXFinalWeb/CasaDollarWeb/CasaDollarWeb/Controllers/GastosController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasaDollarWeb.Controllers
+{
+    public class GastosController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            
+            Dictionary<string, decimal> model = new Dictionary<string, decimal>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:56746/api/casadollar/");
+
+                var responseTask = client.GetAsync("totalgastos");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<Dictionary<string, decimal>>();
+                    readTask.Wait();
+                    
+                    model = readTask.Result;
+                }
+                else
+                {
+                    model = new Dictionary<string, decimal>();
+                    ModelState.AddModelError(string.Empty, "Error en solicitud");
+                }
+            }
+            return View(model);
+        }
+    }
+}
